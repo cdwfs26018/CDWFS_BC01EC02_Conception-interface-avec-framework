3c17326c212db1eb23ecac6b5a7105b8939847b5
CSI-WEB-2 <CSI-WEB-2@campus.local>
2026-01-14 10:57:33 +0100

    biblio footer

--- src/app/_components/footer/footer.ts
+++ src/app/_components/footer/footer.ts
@@ -1,15 +1,29 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import {
+  LucideAngularModule,
+  BookOpen,
+  Package,
+  User,
+  ShoppingCart
+} from 'lucide-angular';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LucideAngularModule],
   templateUrl: './footer.html',
 })
 export class FooterComponent {
   private router = inject(Router);
+
+  icons = {
+    catalogue: BookOpen,
+    box: Package,
+    account: User,
+    cart: ShoppingCart,
+  };
 
   go(path: string) {
     this.router.navigate([path]);
