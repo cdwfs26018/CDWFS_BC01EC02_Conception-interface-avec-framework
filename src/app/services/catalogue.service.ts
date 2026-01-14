c70cce4d5dc1794977c233aaee7e655bc81388ec
CSI-WEB-2 <CSI-WEB-2@campus.local>
2026-01-14 09:57:32 +0100

    ajout product

--- src/app/services/catalogue.service.ts
+++ src/app/services/catalogue.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
 import { Product } from '../models/product';
 import { Category } from '../models/category';
+import { Collection } from '../models/collection';
 
 @Injectable({ providedIn: 'root' })
 export class CatalogueService {
@@ -17,6 +18,18 @@ export class CatalogueService {
   getCategories(): Promise<Category[]> {
     return firstValueFrom(
       this.http.get<Category[]>('/json/categories.json')
+    );
+  }
+
+  getProductByRef(ref: string): Promise<Product | undefined> {
+    return this.getProducts().then(
+      products => products.find(p => p.reference_produit === ref)
+    );
+  }
+
+  getCollections(): Promise<Collection[]> {
+    return firstValueFrom(
+      this.http.get<Collection[]>('/json/collections.json')
     );
   }
 }
